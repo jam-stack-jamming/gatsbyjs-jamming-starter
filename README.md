9056a35631c1134639d23c6db007f1c1fdba39ac
Kalin Chernev <kalin.chernev@gmail.com>
2019-04-13 12:57:45 +0300

    Start the starter

---
+++ README.md
@@ -0,0 +1,40 @@
+# Gatsby.js Jamming Starter
+
+Gatsby.js starter for the meaningful projects which need to be shipped fast.
+
+## Requirements
+
+- Node.js (8.10.x recommended)
+- yarn >= 1.x
+
+You can use [Node Version Manager](https://github.com/creationix/nvm) in order to be able to use several versions of Node.js within your system.
+
+```sh
+# Install node version corresponding to the one defined in .nvmrc
+# Use '--reinstall-packages-from=node' if you want to migrate npm packages from a previous version
+nvm install
+
+# Use the newly installed version
+nvm use
+```
+
+## Getting started
+
+Setup your environment:
+
+```sh
+yarn
+```
+
+## Configuration
+
+Update the following files:
+
+- `package.json`: name and description of the project
+- `config.js`: all configurations centralized
+
+## Development
+
+```sh
+yarn start
+```
---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteTitle: 'JAM Stack Jamming', // Site title.
+  siteTitleAlt: '', // Alternative site title for SEO.
+  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
+  siteUrl: 'https://gatsby-starter.netlify.com', // Domain of your website without pathPrefix.
+  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription: '', // Website description used for RSS feeds/meta description tag.
+  siteRss: '/rss.xml',
+  siteFBAppID: '', // FB Application ID for using app insights
+  googleTagManagerID: '', // GTM tracking ID.
+  disqusShortname: '', // Disqus shortname.
+  netlifyCmsHtmlDescription: 'Gatsby Starter Business Content Manager',
+  userName: '',
+  userTwitter: '',
+  userLocation: '',
+  userDescription: '',
+  copyright: '', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#ffffff', // Used for setting manifest background color.
+};
